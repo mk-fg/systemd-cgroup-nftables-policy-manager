8bb628e43f1b08b38bad39fc198f9db5770a5063
Mike Kazantsev <mk.fraggod@gmail.com>
2021-08-28 15:31:52 +0500

    Add parser for nft commented-out rules to apply/remove

--- scnpm.ml
+++ scnpm.ml
@@ -53,11 +53,12 @@ external journal_match_flush : unit -> unit = "mlj_match_flush"
 
 let journal_wait_us = 3600_000_000 (* don't need a limit for local journal *)
 
+let debug_print line = if !cli_debug then prerr_endline line; flush stdout
+let try_finally f x finally y =
+	let res = try f x with e -> finally y; raise e in finally y; res
+
 
 let tail_journal () =
-	let try_finally f x finally y =
-		let res = try f x with e -> finally y; raise e in finally y; res in
-	let debug_print line = if !cli_debug then print_endline line; flush stdout in
 
 	let init () =
 		journal_open journal_fields;
@@ -90,20 +91,59 @@ let tail_journal () =
 			let update = journal_wait journal_wait_us in
 			if update then tail_parse () else tail_parse_wait () in
 		let rec tail_iter n =
-			try Option.some (Queue.take tail_queue)
+			try Queue.take tail_queue |> Option.some
 			with Queue.Empty -> tail_parse_wait (); tail_iter n in
 		Stream.from tail_iter in
 
-	let rec run_tail_loop () =
-		let jr = Stream.next tail in
-		debug_print (Printf.sprintf "journal :: event u=%s uu=%s" jr.u jr.uu);
-		run_tail_loop () in
+	init (); tail, cleanup
 
-	debug_print "Starting journal-parser loop...";
-	init (); try_finally run_tail_loop () cleanup ()
+(* let run () =
+ * 	let tail, tail_cleanup = tail_journal () in
+ * 	let rec run_tail_loop () =
+ * 		let jr = Stream.next tail in
+ * 		debug_print (Printf.sprintf "journal :: event u=%s uu=%s" jr.u jr.uu);
+ * 		run_tail_loop () in
+ * 	try_finally run_tail_loop () tail_cleanup () *)
 
 
-let () = Unix.handle_unix_error tail_journal ()
+let run () =
+	let unit_rules =
+		let table = Hashtbl.create 8 in
+		let re_start = Str.regexp
+			"^#+ *\\([^ ]+\\.[a-zA-Z]+\\) +:: +add +rule +\\(.+\\)$" in
+		let re_cont = Str.regexp "^#+ \\(.*\\)$" in
+		let rec parse_config fn =
+			let src = open_in fn in
+			let rec read_line cg rule =
+				let cg, rule_start =
+					if rule = "" then "", ""
+					else if Str.last_chars rule 1 = "\\"
+						then cg, Str.first_chars rule (String.length rule - 1) |> String.trim
+						else (Hashtbl.add table cg rule; "", "") in
+				let line =
+					try (input_line src) ^ "\n"
+					with End_of_file -> "" in
+				if line <> "" then
+					let line = String.trim line in
+					let cg, rule_start =
+						if rule_start <> "" then (
+							if Str.string_match re_cont line 0
+								then cg, rule_start ^ " " ^ (Str.matched_group 1 line |> String.trim)
+								else raise (Failure ( Printf.sprintf "Broken rule \
+									continuation in [ %s ]: '%s' + '%s'" fn rule_start line )) )
+						else if Str.string_match re_start line 0
+							then Str.matched_group 1 line, Str.matched_group 2 line
+						else "", "" in
+					read_line cg rule_start in
+			read_line "" "" in
+		List.iter parse_config !cli_nft_configs; table in
+	debug_print ( Printf.sprintf
+		"config :: loaded rules: %d" (Hashtbl.length unit_rules) );
+	Hashtbl.iter (fun k v -> debug_print (
+		Printf.sprintf "config :: [ %s ] rule %s" k v )) unit_rules
+
+
+let () = Unix.handle_unix_error run ()
 	(* let sig_done = Sys.Signal_handle (fun sig_n -> exit 0) in
 	 * 	Sys.set_signal Sys.sigterm sig_done;
 	 * 	Sys.set_signal Sys.sigint sig_done;
